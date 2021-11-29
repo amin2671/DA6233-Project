214be517f0124c47cb91f6c15a877161b0d160ff
Andrew Min <76923974+amin2671@users.noreply.github.com>
2021-11-28 21:53:32 -0600

    updated and added graphs

--- epa graphs/epa graphs.R
+++ epa graphs/epa graphs.R
@@ -2,68 +2,33 @@ library(tidyverse); library(plotly); theme_set(theme_minimal())
 
 setwd("~/HOMEWORK FOLDER - FALL 2021/DA 6233/PROJECT/nfl-big-data-bowl-2021")
 
-playsOut = read.csv("out.csv", header=TRUE) 
-# out.csv is just plays.csv but with more columns that I needed
+pbp2018 <- read_csv("https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2018.csv.gz")
+plays = read.csv("plays.csv", header=TRUE)
 
-########## SIDE NOTE: NEGATIVE EPA = BETTER DEFENSE; POSITIVE EPA = BETTER OFFENSE
 
-# Not in possession
-DefenseEPA_NonPossession <- playsOut %>%
-  group_by(offenseFormation) %>%
-  filter(nonPossession == "PHI") %>%
-  mutate(meanEPA = mean(epa))
+topReceiversDATA <- pbp2018 %>%
+  filter(receiver %in% c("Z.Ertz", "A.Cooper"), week <= 9) %>%
+  group_by(week, receiver) %>%
+  summarize(mean_epa = mean(epa, na.rm = TRUE))
 
-PlayTypeEPA_NonPossession <- playsOut %>%
-  group_by(playType) %>%
-  filter(nonPossession == "PHI") %>%
-  mutate(meanEPA = mean(epa))
+topRushersDATA <- pbp2018 %>%
+  filter(rusher %in% c("E.Elliott", "W.Smallwood"), week <= 9) %>%
+  group_by(week, rusher) %>%
+  summarize(mean_epa = mean(epa, na.rm = TRUE))
 
-# In possession
-OffenseEPA_Possession <- playsOut %>%
+
+OffenseEPA <- plays %>%
   group_by(offenseFormation) %>%
   filter(possessionTeam == "PHI") %>%
   mutate(meanEPA = mean(epa))
 
-PlayTypeEPA_Possession <- playsOut %>%
+PlayTypeEPA <- plays %>%
   group_by(playType) %>%
   filter(possessionTeam == "PHI") %>%
   mutate(meanEPA = mean(epa))
 
-
-
-# EPA of Offensive formations while not in possession
-PLOT_Defense_Non <- DefenseEPA_NonPossession %>%
-  ggplot(aes(x=offenseFormation, y=epa)) + 
-  labs(title="EPA for Offensive Formations while not in Possession",
-       x = "Offensive Formations", y = "EPA") +
-  geom_boxplot() + 
-  geom_point(shape=17, size=1, color="red", aes(x=offenseFormation, y=meanEPA)) +
-  theme(legend.position = "none") +
-  theme(plot.title = element_text(hjust = 0.5))
-
-ggplotly(PLOT_Defense_Non)
-
-
-# EPA for play types while not in possession
-PLOT_PlayType_Non <- PlayTypeEPA_NonPossession %>% 
-  ggplot(aes(x=playType, y=epa)) + 
-  labs(title="EPA for Play Types while not in Possession",
-       x = "Play Type", y = "EPA") +
-  geom_boxplot() + 
-  geom_point(shape=17, size=1, color="red", aes(x=playType, y=meanEPA)) +
-  theme(legend.position = "none") + 
-  scale_x_discrete(breaks=c("play_type_pass","play_type_sack","play_type_unknown"), 
-                   labels=c("Pass", "Sack", "No Play")) +
-  theme(plot.title = element_text(hjust = 0.5))
-
-ggplotly(PLOT_PlayType_Non)
-
-# ABOVE IS FOR NONPOSSESSION
-######################################################
-# BELOW IS FOR POSSESSION
-
-# EPA for offensive formations while in possession
-PLOT_Offense_Pos <- OffenseEPA_Possession %>%
+# EPA for offensive formations
+PLOT_Offense_Pos <- OffenseEPA %>%
   ggplot(aes(x=offenseFormation, y=epa)) + 
   labs(title="EPA for Offensive Formations while in Possession",
        x = "Offensive Formations", y = "EPA") +
@@ -76,8 +41,8 @@ PLOT_Offense_Pos <- OffenseEPA_Possession %>%
 ggplotly(PLOT_Offense_Pos)
 
 
-# EPA for play types while in possession
-PLOT_PlayType_Pos <- PlayTypeEPA_Possession %>% 
+# EPA for play types
+PLOT_PlayType_Pos <- PlayTypeEPA %>% 
   ggplot(aes(x=playType, y=epa)) + 
   labs(title="EPA for Play Types while in Possession",
        x = "Play Type", y = "EPA") +
@@ -89,3 +54,47 @@ PLOT_PlayType_Pos <- PlayTypeEPA_Possession %>%
   theme(plot.title = element_text(hjust = 0.5))
 
 ggplotly(PLOT_PlayType_Pos)
+
+
+# Average EPA per week for Top Receiver for Cowboys vs Eagles
+receivers <- topReceiversDATA %>% 
+  ggplot(aes(x = week, y = mean_epa, color = receiver)) +
+  geom_line(size = .75) +
+  geom_hline(yintercept = 0, size = .5, color = "black") +
+  labs(
+    x = "Weeks",
+    y = "EPA (Average)",
+    title = "Comparison of Cooper (DAL) vs Ertz (PHI)"
+  ) +
+  geom_point(size = 1.5) +
+  scale_x_continuous(breaks = seq(1, 9, 1)) +
+  scale_color_manual(values=c('#003594','#004C54')) +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "top"
+  ) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplotly(receivers)
+
+
+# Average EPA per week for Top Rusher for Cowboys vs Eagles
+rushers <- topRushersDATA %>%
+  ggplot(aes(x = week, y = mean_epa, color = rusher)) +
+  geom_line(size = .75) +
+  geom_hline(yintercept = 0, size = .5, color = "black") +
+  labs(
+    x = "Weeks",
+    y = "EPA (Average)",
+    title = "Comparison of Elliott (DAL) vs Smallwood (PHI)"
+  ) +
+  geom_point(size = 1.5) +
+  scale_x_continuous(breaks = seq(1, 9, 1)) +
+  scale_color_manual(values=c('#003594','#004C54')) +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "top"
+  ) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplotly(rushers)
